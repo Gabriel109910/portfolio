429de0adc8e7b82f4406447b7a1abd574d9aa5a4
Gabriel109910 <165337663+Gabriel109910@users.noreply.github.com>
2024-08-29 14:01:31 -0400

    Update Portfolio Project - Data Cleaning.sql

--- Portfolio Project - Data Cleaning.sql
+++ Portfolio Project - Data Cleaning.sql
@@ -3,10 +3,6 @@
 -- https://www.kaggle.com/datasets/swaptr/layoffs-2022
 
 
-
-
-
-
 SELECT * 
 FROM world_layoffs.layoffs;
 
@@ -58,14 +54,11 @@ FROM (
 WHERE 
 	row_num > 1;
     
--- let's just look at oda to confirm
 SELECT *
 FROM world_layoffs.layoffs_staging
 WHERE company = 'Oda'
 ;
--- it looks like these are all legitimate entries and shouldn't be deleted. We need to really look at every single row to be accurate
 
--- these are our real duplicates 
 SELECT *
 FROM (
 	SELECT company, location, industry, total_laid_off,percentage_laid_off,`date`, stage, country, funds_raised_millions,
@@ -78,9 +71,8 @@ FROM (
 WHERE 
 	row_num > 1;
 
--- these are the ones we want to delete where the row number is > 1 or 2or greater essentially
 
--- now you may want to write it like this:
+
 WITH DELETE_CTE AS 
 (
 SELECT *
@@ -111,10 +103,9 @@ WHERE (company, location, industry, total_laid_off, percentage_laid_off, `date`,
 	FROM DELETE_CTE
 ) AND row_num > 1;
 
--- one solution, which I think is a good one. Is to create a new column and add those row numbers in. Then delete where row numbers are over 2, then delete that column
--- so let's do it!!
+-- One solution, which I think is a good one, is to create a new column and add those row numbers.
 
-ALTER TABLE world_layoffs.layoffs_staging ADD row_num INT;
+
 
 
 SELECT *
@@ -160,7 +151,7 @@ SELECT `company`,
 	FROM 
 		world_layoffs.layoffs_staging;
 
--- now that we have this we can delete rows were row_num is greater than 2
+
 
 DELETE FROM world_layoffs.layoffs_staging2
 WHERE row_num >= 2;
@@ -176,7 +167,6 @@ WHERE row_num >= 2;
 SELECT * 
 FROM world_layoffs.layoffs_staging2;
 
--- if we look at industry it looks like we have some null and empty rows, let's take a look at these
 SELECT DISTINCT industry
 FROM world_layoffs.layoffs_staging2
 ORDER BY industry;
@@ -187,26 +177,22 @@ WHERE industry IS NULL
 OR industry = ''
 ORDER BY industry;
 
--- let's take a look at these
 SELECT *
 FROM world_layoffs.layoffs_staging2
 WHERE company LIKE 'Bally%';
--- nothing wrong here
+
+
 SELECT *
 FROM world_layoffs.layoffs_staging2
 WHERE company LIKE 'airbnb%';
 
--- it looks like airbnb is a travel, but this one just isn't populated.
--- I'm sure it's the same for the others. What we can do is
--- write a query that if there is another row with the same company name, it will update it to the non-null industry values
--- makes it easy so if there were thousands we wouldn't have to manually check them all
 
--- we should set the blanks to nulls since those are typically easier to work with
+
 UPDATE world_layoffs.layoffs_staging2
 SET industry = NULL
 WHERE industry = '';
 
--- now if we check those are all null
+
 
 SELECT *
 FROM world_layoffs.layoffs_staging2
@@ -214,7 +200,7 @@ WHERE industry IS NULL
 OR industry = ''
 ORDER BY industry;
 
--- now we need to populate those nulls if possible
+-
 
 UPDATE layoffs_staging2 t1
 JOIN layoffs_staging2 t2
@@ -223,7 +209,7 @@ SET t1.industry = t2.industry
 WHERE t1.industry IS NULL
 AND t2.industry IS NOT NULL;
 
--- and if we check it looks like Bally's was the only one without a populated row to populate this null values
+
 SELECT *
 FROM world_layoffs.layoffs_staging2
 WHERE industry IS NULL 
@@ -232,7 +218,7 @@ ORDER BY industry;
 
 -- ---------------------------------------------------
 
--- I also noticed the Crypto has multiple different variations. We need to standardize that - let's say all to Crypto
+
 SELECT DISTINCT industry
 FROM world_layoffs.layoffs_staging2
 ORDER BY industry;
@@ -247,12 +233,12 @@ FROM world_layoffs.layoffs_staging2
 ORDER BY industry;
 
 -- --------------------------------------------------
--- we also need to look at 
+
 
 SELECT *
 FROM world_layoffs.layoffs_staging2;
 
--- everything looks good except apparently we have some "United States" and some "United States." with a period at the end. Let's standardize this.
+
 SELECT DISTINCT country
 FROM world_layoffs.layoffs_staging2
 ORDER BY country;
@@ -260,21 +246,20 @@ ORDER BY country;
 UPDATE layoffs_staging2
 SET country = TRIM(TRAILING '.' FROM country);
 
--- now if we run this again it is fixed
+
 SELECT DISTINCT country
 FROM world_layoffs.layoffs_staging2
 ORDER BY country;
 
 
--- Let's also fix the date columns:
 SELECT *
 FROM world_layoffs.layoffs_staging2;
 
--- we can use str to date to update this field
+
 UPDATE layoffs_staging2
 SET `date` = STR_TO_DATE(`date`, '%m/%d/%Y');
 
--- now we can convert the data type properly
+
 ALTER TABLE layoffs_staging2
 MODIFY COLUMN `date` DATE;
 
@@ -289,9 +274,7 @@ FROM world_layoffs.layoffs_staging2;
 -- 3. Look at Null Values
 
 -- the null values in total_laid_off, percentage_laid_off, and funds_raised_millions all look normal. I don't think I want to change that
--- I like having them null because it makes it easier for calculations during the EDA phase
-
--- so there isn't anything I want to change with the null values
+-- I like having them null because it makes it easier for calculations during the EDA phase, so there isn't anything I want to change with the null values
 
 
 
@@ -308,7 +291,7 @@ FROM world_layoffs.layoffs_staging2
 WHERE total_laid_off IS NULL
 AND percentage_laid_off IS NULL;
 
--- Delete Useless data we can't really use
+
 DELETE FROM world_layoffs.layoffs_staging2
 WHERE total_laid_off IS NULL
 AND percentage_laid_off IS NULL;
